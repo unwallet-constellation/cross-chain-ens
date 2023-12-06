a0ea3d45d5989b18f1951f9be845b0922fcf68f5
Dennis Zoma <dennis@scio.xyz>
2023-12-06 13:55:04 +0100

    chore(scripts): Minor refactorings

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,10 @@ HUB_RPC_URL=avalancheFuji
 SPOKE_CHAIN_IDS=(80001 420)
 SPOKE_RPC_URLS=(polygonMumbai optimismGoerli)
 
+### Smart Contracts ###
+declare -a HUB_CONTRACT_NAMES=("ENSRegistryCCIP" "FIFSRegistrarCCIP" "ReverseRegistrarCCIP" "PublicResolverCCIP")
+declare -a SPOKE_CONTRACT_NAMES=("xcENSRegistry" "xcFIFSRegistrar" "xcReverseRegistrar" "xcPublicResolver")
+
 
 ########################
 ##       CHECKS       ##
@@ -37,18 +41,15 @@ if ! command -v forge &> /dev/null; then
   exit 1
 fi
 
-# Parse arguments
-DRY_RUN=0
-if [ -n "${1-}" ] && [ "$1" == "--dry-run" ]; then
-  DRY_RUN=1
-fi
-
+# Parse arguments:
+#   --dry-run: Prints previous deployment addresses without executing the deployment
+[[ "${1-}" == "--dry-run" ]] && DRY_RUN=1 || DRY_RUN=0
 
 ########################
 ##       SCRIPT       ##
 ########################
 
-# Helper to get deployed contract addresses from the run-latest.json file created by forge
+# 0. Helper to get & store deployed contract addresses from the `run-latest.json` file broadcasted by forge
 declare -A contractAddresses
 function exportContractAddress() {
   local chainId=$1
@@ -58,36 +59,34 @@ function exportContractAddress() {
   contractAddresses[$contractName,$chainId]=$contractAddress
 }
 
-# Deploy the hub contracts
+# 1. Deploy the hub contracts
 if [ $DRY_RUN -eq 0 ]; then
   forge script ./script/Deploy.s.sol:DeployHub -v --broadcast --rpc-url $HUB_RPC_URL --sig "run(uint64)" -- $HUB_CHAIN_ID
 fi
 
-# Export the hub contract addresses
-declare -a hubContractNames=("ENSRegistryCCIP" "FIFSRegistrarCCIP" "ReverseRegistrarCCIP" "PublicResolverCCIP")
-for name in "${hubContractNames[@]}"; do exportContractAddress $HUB_CHAIN_ID $name; done
+# 2. Export the hub contract addresses
+for name in "${HUB_CONTRACT_NAMES[@]}"; do exportContractAddress $HUB_CHAIN_ID $name; done
 
-# Deploy the spoke contracts
+# 3. Deploy the spoke contracts
 if [ $DRY_RUN -eq 0 ]; then
   for i in "${!SPOKE_CHAIN_IDS[@]}"; do
     forge script ./script/Deploy.s.sol:DeploySpoke -v --broadcast --rpc-url ${SPOKE_RPC_URLS[$i]} --sig "run(uint64, uint64)" -- ${SPOKE_CHAIN_IDS[$i]} $HUB_CHAIN_ID
   done
 fi
 
-# Export the spoke contract addresses
-declare -a spokeContractNames=("xcENSRegistry" "xcFIFSRegistrar" "xcReverseRegistrar" "xcPublicResolver")
+# 4. Export the spoke contract addresses
 for i in "${!SPOKE_CHAIN_IDS[@]}"; do
-  for name in "${spokeContractNames[@]}"; do exportContractAddress ${SPOKE_CHAIN_IDS[$i]} $name; done
+  for name in "${SPOKE_CONTRACT_NAMES[@]}"; do exportContractAddress ${SPOKE_CHAIN_IDS[$i]} $name; done
 done
 
-# Build JSON output
+# 5. Build JSON output
 declare -A jsonOutput
 for key in "${!contractAddresses[@]}"; do
   IFS=',' read -r -a array <<< "$key"
   jsonOutput[${array[0]}]=${jsonOutput[${array[0]}]-}"\n    ${array[1]}: \"${contractAddresses[$key]}\","
 done
 
-# Print JSON output
+# 6. Print JSON output
 echo -e "\nDeployed contract addresses (JSON):"
 for key in "${!jsonOutput[@]}"; do
   echo -e "  $key: {${jsonOutput[$key]%,}\n  },"
